3dab77d3cde952c6dd8e9347715529b6e7ad5cad
Manuel Jimenez <manueljimenez0320@gmail.com>
2024-11-01 16:21:33 -0400

    tryting to implement twosum by using unorderedmap

--- unorderedtest.cpp
+++ unorderedtest.cpp
@@ -2,17 +2,30 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <iterator>
 
-std::vector<int> getIndexes(const std::vector<int>& nums)
+std::vector<int> getIndexes(const std::vector<int>& nums, int target)
 {
-    std::vector<int> newvec;
-    return newvec;
+    std::unordered_map<int, int> pastnums;
+    auto beginit = nums.begin();
+
+    for (std::vector<int>::const_iterator it = nums.begin(); it != nums.end(); ++it)
+    {
+        if (pastnums.count(target - *it))
+            return {pastnums[target - *it], it - nums.begin()}; //pastnumes[target-*it] gives the value (index) for the key since we found the index
+        pastnums.insert({target - *it, it - nums.begin()});
+    }
+    return {-1, -1};
 }
 
 int main()
 {
+    int targetnum = 15;
+    std::vector<int> testOne{5, 3, 6, 4, 1, 9, 19};
+    std::vector<int> indexes = getIndexes(testOne, targetnum);
 
-    std::unordered_map<std::string, std::string> mapOne;
-
-
+    std::cout << "The indexes that add up to " << targetnum << " are: ";
+    for (auto x: indexes)
+        std::cout << x << " ";
+    std::cout << std::endl;
 }
